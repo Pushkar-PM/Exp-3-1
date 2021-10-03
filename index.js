fdbcb7ed5e56db4434c216b73dbd3e37dda2f849
Pushkar-PM <80151770+Pushkar-PM@users.noreply.github.com>
2021-10-04 00:26:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+function validateForm() {
+    // Validation
+    const mytshirt = [ 
+        document.forms["tshirtForm"]["tagline"].value, 
+        document.forms["tshirtForm"]["color"].value, 
+        document.forms["tshirtForm"]["size"].value, 
+        document.forms["tshirtForm"]["date"].value,
+        document.forms["tshirtForm"]['quantity'].value,
+    ];
+    
+    const Empty = Object.values(mytshirt).some( (element) => element === "");
+    
+    if (Empty) {
+        alert("Please fill in all fields"); 
+        return;
+    }
+
+    const q=document.querySelector(".ch");
+    console.log(q.checked);
+    if(q.checked==false){
+        alert("Please accept our terms");
+        return;
+    }
+    
+    // Bill
+    const bill = `RECEIPT \nYour Coool Tagline: ${mytshirt[0]}\nColor: ${mytshirt[1]}\nSize: ${mytshirt[2]} \nDate: ${mytshirt[3]} \nQuantity: ${mytshirt[4]}`;
+    
+    alert(bill);
+    }
+    
+    const form = document.getElementById("tshirtForm"); 
+
+    form.addEventListener("submit", (event) => {
+        // stop form submission 
+        event.preventDefault();
+    });
