08b539de49519173b230cff2801c104e2f29bb7b
Pushkar-PM <80151770+Pushkar-PM@users.noreply.github.com>
2021-10-04 15:16:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function validateForm() {
         document.forms["tshirtForm"]["size"].value, 
         document.forms["tshirtForm"]["date"].value,
         document.forms["tshirtForm"]['quantity'].value,
+        document.forms["tshirtForm"]["mobile"].value,
     ];
     
     const Empty = Object.values(mytshirt).some( (element) => element === "");
@@ -16,11 +17,18 @@ function validateForm() {
     }
 
     const q=document.querySelector(".ch");
+    const y=document.forms["tshirtForm"]["mobile"].value;
+    if(y.length!=10||parseInt(y)==NaN){
+        alert("Invalid mobile number");
+        return;
+    }
+
     console.log(q.checked);
     if(q.checked==false){
         alert("Please accept our terms");
         return;
     }
+
     
     // Bill
     const bill = `RECEIPT \nYour Coool Tagline: ${mytshirt[0]}\nColor: ${mytshirt[1]}\nSize: ${mytshirt[2]} \nDate: ${mytshirt[3]} \nQuantity: ${mytshirt[4]}`;
